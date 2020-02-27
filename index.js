d396f659932a69838d74f97e5a5353ccb60ab4ad
github username <github email address>
2020-02-27 14:38:16 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+    const win = array.find(year => year.result === 'W')
+    if (win) {
+      return win.year
+    } else {
+      return win
+    }
+}
